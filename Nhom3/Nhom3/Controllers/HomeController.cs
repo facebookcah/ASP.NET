084bfe158ff3515e02689be5ff042990d3524479
PhongND <duyphong05012000@gmail.com>
2021-11-26 16:24:19 +0700

    push 26/11

---
+++ Nhom3/Nhom3/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+
+
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+
+
+            return View();
+        }
+        public ActionResult EditUser()
+        {
+
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Users()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+    }
+}
